d8144e7ca6aab842fb95f29241245c96a18fcce1
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-10-14 03:53:18 +0000

    Added validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,16 @@
 class Listing < ActiveRecord::Base
   if Rails.env.production?
+    
     has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing.jpg"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    
+    validates :name, :description, :price, :image, presence: true
   else
     has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing.jpg"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    
+    validates :name, :description, :price, presence: true
+    validates :price, numericality: { greater_than: 0}
+    validates_attachment_presence :image
   end
 end
