a733e8ac026d0286b18bcd405d8623d542cc6161
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-10-14 04:23:22 +0000

    Linked Users to Listings

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   validates :name, :email, :password, presence: true
+  
+  has_many :listing, dependent: :destroy
 end
